f81ada2fdbdd6bbf567f12dd23b1f10c2bafd2e7
Tom Whittington <46750921+ThomasWhittington@users.noreply.github.com>
2025-10-23 21:18:27 +0100

    Add dummy game loading functionality
    Implemented `LoadDummyGames` endpoint to load dummy game data from a JSON file. Added `LoadDummyData` method in `GameStateService` to handle deserialization and state population. Included `dummyState.json` with sample game data.

---
+++ Clocktower.Server/Game/Endpoints/LoadDummyGames.cs
@@ -0,0 +1,18 @@
+﻿namespace Clocktower.Server.Game.Endpoints;
+
+[UsedImplicitly]
+public class LoadDummyGames : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app) => app
+        .MapPost("/load", Handle)
+        .WithSummary("Loads dummy data from saved json file");
+
+    private static Results<Ok<string>, BadRequest<string>> Handle(GameStateService gameStateService)
+    {
+        var result = gameStateService.LoadDummyData();
+
+        return result.success
+            ? TypedResults.Ok(result.message)
+            : TypedResults.BadRequest(result.message);
+    }
+}d
