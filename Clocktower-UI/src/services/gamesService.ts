d4287a07ae9474262d099cd9c1a49f6b6b492a91
Tom Whittington <46750921+ThomasWhittington@users.noreply.github.com>
2025-10-31 16:01:13 +0000

    integrate OpenAPI client methods in `gamesService` and update launch settings

--- Clocktower-UI/src/services/gamesService.ts
+++ Clocktower-UI/src/services/gamesService.ts
@@ -1,47 +1,44 @@
 ﻿import {
     type GameState,
+    mapToGameState,
 } from "../types/game.ts";
+import {
+    getGameApi,
+    getGamesApi,
+    loadDummyGamesApi,
+    startGameApi
+} from "../openApi";
 
 
 async function getGame(id: string): Promise<GameState> {
-    /*
-      const {
-          data,
-          error
-      } = await client["/api/games/{gameId}"].GET({
-          params: {
-              path: {"gameId": id}
-          }
-      });
-  
-      if (error) {
-          console.error('Failed to get game:', error);
-          throw new Error('Failed to get game');
-      }
-  
-      if (data) {
-          return mapToGameState(data);
-      }
-      return {
-          id: id,
-          players: [],
-          maxPlayers: 0,
-          isFull: false
-      };*/
+
+    const {
+        data,
+        error
+    } = await getGameApi({path: {"gameId": id}});
+
+    if (error) {
+        console.error('Failed to get game:', error);
+        throw new Error('Failed to get game');
+    }
+
+    if (data) {
+        return mapToGameState(data);
+    }
     return {
         id: id,
-        isFull: false,
+        players: [],
         maxPlayers: 0,
-        players: []
+        isFull: false
     };
 }
 
 async function getGames(): Promise<GameState[]> {
-    /*
+
     const {
         data,
         error
-    } = await client["/api/games"].GET();
+    } = await getGamesApi();
 
     if (error) {
         console.error('Failed to fetch games:', error);
@@ -49,61 +46,42 @@ async function getGames(): Promise<GameState[]> {
     }
 
     return data?.map(mapToGameState) ?? [];
-    */
-    return [];
 }
 
 async function loadDummyData(): Promise<string | undefined> {
-    /* const {
-         data,
-         error
-     } = await client["/api/games/load"].POST();
- 
-     if (error) {
-         console.error('Failed to load dummy data:', error);
-         throw new Error('Failed to load dummy data');
-     }
- 
-     return data;
-     */
-    return '';
+    const {
+        data,
+        error
+    } = await loadDummyGamesApi();
 
+    if (error) {
+        console.error('Failed to load dummy data:', error);
+        throw new Error('Failed to load dummy data');
+    }
+
+    return data;
 }
 
 async function startGame(id: string): Promise<GameState> {
-    /*
-     const {
-         data,
-         error
-     } = await client["/api/games/{gameId}/start"].POST({
-         params: {
-             path: {"gameId": id}
-         }
-     });
- 
-     if (error) {
-         console.error('Failed to start game:', error);
-         throw new Error('Failed to start game');
-     }
- 
-     if (data) {
-         return mapToGameState(data);
-     }
-     return {
-         id: id,
-         players: [],
-         maxPlayers: 0,
-         isFull: false
-     };
-     */
+
+    const {
+        data,
+        error
+    } = await startGameApi({path: {"gameId": id}});
+    if (error) {
+        console.error('Failed to start game:', error);
+        throw new Error('Failed to start game');
+    }
+
+    if (data) {
+        return mapToGameState(data);
+    }
     return {
         id: id,
-        isFull: false,
+        players: [],
         maxPlayers: 0,
-        players: []
-
+        isFull: false
     };
-
 }
 
 export const gamesService = {
