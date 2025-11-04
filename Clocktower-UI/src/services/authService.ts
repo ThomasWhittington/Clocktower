24be528e23a3415f69db3e6d27ff8e61e9f81a8c
Tom Whittington <46750921+ThomasWhittington@users.noreply.github.com>
2025-11-04 22:59:32 +0000

    Removed uneeded fetch

--- Clocktower-UI/src/services/authService.ts
+++ Clocktower-UI/src/services/authService.ts
@@ -3,18 +3,6 @@
 } from '../types/auth';
 
 export const authService = {
-    async getUserData(key: string): Promise<User> {
-        const response = await fetch(`http://localhost:5120/api/discord/auth/data/${key}`); //TODO use openapi
-        if (!response.ok) {
-            throw new Error('Failed to retrieve auth data');
-        }
-        return response.json();
-    },
-
-    saveUser(user: User): void {
-        localStorage.setItem('user', JSON.stringify(user));
-    },
-
     getUser(): User | null {
         const userStr = localStorage.getItem('user');
         return userStr ? JSON.parse(userStr) : null;
