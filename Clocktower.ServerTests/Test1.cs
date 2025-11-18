f89c586219d2653b229e900e8247ecbc5d87c172
Tom Whittington <46750921+ThomasWhittington@users.noreply.github.com>
2025-11-18 17:11:40 +0000

    test commit 2

---
+++ Clocktower.ServerTests/Test1.cs
@@ -0,0 +1,13 @@
+﻿using FluentAssertions;
+
+namespace Clocktower.ServerTests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        false.Should().BeTrue();
+    }
+}
