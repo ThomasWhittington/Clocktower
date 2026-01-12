885ea47538cc7401f00bca911a60ab89f28d1f59
Tom Whittington <46750921+ThomasWhittington@users.noreply.github.com>
2026-01-12 00:14:55 +0000

    Added unique curve ids

--- Clocktower-UI/src/components/tokens/RoleName.tsx
+++ Clocktower-UI/src/components/tokens/RoleName.tsx
@@ -1,14 +1,17 @@
-﻿export const RoleName = ({roleName}: { roleName: string }) => {
+﻿import {useId} from 'react';
+
+export const RoleName = ({roleName}: { roleName: string }) => {
+    const curveId = useId();
         const fontSize = (roleName && roleName.length > 10 ? "90%" : "110%");
         return (
             <svg viewBox="0 0 150 150" className="token-role-name">
                 <path
                     d="M 13 75 C 13 160, 138 160, 138 75"
-                    id="curve"
+                    id={curveId}
                     fill="transparent"
                 />
                 <text width="150" x="66.6%" text-anchor="middle" className="label" fontSize={fontSize}>
-                    <textPath xlinkHref="#curve">
+                    <textPath href={`#${curveId}`}>
                         {roleName}
                     </textPath>
                 </text>
