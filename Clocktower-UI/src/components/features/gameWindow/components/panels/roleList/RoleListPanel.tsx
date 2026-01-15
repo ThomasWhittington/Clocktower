ee5474b1795a82d52a3f50e2eba007d14d92cf61
Tom Whittington <46750921+ThomasWhittington@users.noreply.github.com>
2026-01-15 17:07:06 +0000

    Added RoleListPanel.tsx

--- Clocktower-UI/src/components/features/gameWindow/components/panels/roleList/RoleListPanel.tsx
+++ Clocktower-UI/src/components/features/gameWindow/components/panels/roleList/RoleListPanel.tsx
@@ -1,4 +1,7 @@
 ﻿import {BasePanel} from "@/components/ui";
+import {useServerHub} from "@/hooks";
+import {RoleListRecord} from "./RoleListRecord";
+import {useDiscordTown} from "@/components/features/discordTownPanel/hooks";
 
 interface RoleListPanelProps {
     isOpen: boolean;
@@ -6,9 +9,49 @@ interface RoleListPanelProps {
 }
 
 export const RoleListPanel = ({isOpen, onClose}: RoleListPanelProps) => {
-    return (
-        <BasePanel title="Role List" isOpen={isOpen} onClose={onClose} className="role-list-panel">
-            <h2>dummy</h2>
-        </BasePanel>
-    )
-};
+        const {script} = useServerHub();
+        const {discordTown} = useDiscordTown();
+        const players = discordTown?.gameUsers || [];
+        return (
+            <BasePanel title="Role List" isOpen={isOpen} onClose={onClose}>
+                <div className="role-list-container">
+                    <div className="team townsfolk">
+                        <aside className="role-aside"><h4>Townsfolk</h4></aside>
+                        <ul className="role-list">
+                            {script?.townsfolk.map((role) => (
+                                <RoleListRecord key={role.id} role={role} players={players}/>
+                            ))}
+                        </ul>
+                    </div>
+
+                    <div className="team outsider">
+                        <aside className="role-aside"><h4>Outsider</h4></aside>
+                        <ul className="role-list">
+                            {script?.outsiders.map((role) => (
+                                <RoleListRecord key={role.id} role={role} players={players}/>
+                            ))}
+                        </ul>
+                    </div>
+
+                    <div className="team minion">
+                        <aside className="role-aside"><h4>Minion</h4></aside>
+                        <ul className="role-list">
+                            {script?.minions.map((role) => (
+                                <RoleListRecord key={role.id} role={role} players={players}/>
+                            ))}
+                        </ul>
+                    </div>
+
+                    <div className="team demon">
+                        <aside className="role-aside"><h4>Demon</h4></aside>
+                        <ul className="role-list">
+                            {script?.demons.map((role) => (
+                                <RoleListRecord key={role.id} role={role} players={players}/>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </BasePanel>
+        )
+    }
+;
---
+++ Clocktower-UI/src/components/features/gameWindow/components/panels/roleList/RoleListRecord.tsx
@@ -0,0 +1,25 @@
+﻿import {Role, User} from "@/types";
+
+interface RoleListRecordProps {
+    role?: Role;
+    players: User[];
+}
+
+export const RoleListRecord = ({role, players}: RoleListRecordProps) => {
+        const playerNames = players
+            .filter(player => player.role?.id === role?.id)
+            .map(p => p.name)
+            .join(', ');
+
+        return (
+            <li className="role-record townsfolk">
+                <span className="role-icon" style={{backgroundImage: `url(/tokenParts/roles/${role?.id}.png)`}}/>
+                <div className="role">
+                    <span className="role-player">{playerNames}</span>
+                    <span className="role-name">{role?.name}</span>
+                    <span className="role-ability">{role?.description}</span>
+                </div>
+            </li>
+        )
+    }
+;
