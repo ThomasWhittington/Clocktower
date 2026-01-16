39df233fa6dbb5f319d219813f9235b0073a5254
Tom Whittington <46750921+ThomasWhittington@users.noreply.github.com>
2026-01-16 01:36:30 +0000

    Added players to RoleListRecord.tsx

--- Clocktower-UI/src/components/features/gameWindow/components/panels/roleList/RoleListRecord.tsx
+++ Clocktower-UI/src/components/features/gameWindow/components/panels/roleList/RoleListRecord.tsx
@@ -1,4 +1,5 @@
 ﻿import {Role, User} from "@/types";
+import {TokenRoleIcon} from "@/components/tokens";
 
 interface RoleListRecordProps {
     role?: Role;
@@ -6,16 +7,20 @@ interface RoleListRecordProps {
 }
 
 export const RoleListRecord = ({role, players}: RoleListRecordProps) => {
-        const playerNames = players
-            .filter(player => player.role?.id === role?.id)
-            .map(p => p.name)
-            .join(', ');
+    if (!role) return;
+    const playersWithRole = players.filter(player => player.role?.id === role?.id);
 
         return (
             <li className="role-record townsfolk">
-                <span className="role-icon" style={{backgroundImage: `url(/tokenParts/roles/${role?.id}.png)`}}/>
+                <TokenRoleIcon role={role} className="role-icon"/>
                 <div className="role">
-                    <span className="role-player">{playerNames}</span>
+                    <span className="role-player">
+                        {playersWithRole.map((player, index) =>
+                            <small className={`${player.isDead ? 'dead' : ''}`} key={player.id}>
+                                {player.name + (playersWithRole.length > index + 1 ? "," : "")}
+                            </small>
+                        )}
+                    </span>
                     <span className="role-name">{role?.name}</span>
                     <span className="role-ability">{role?.description}</span>
                 </div>
