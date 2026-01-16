e1de95cd0cab59861ef695a6d542f952bbfa62f1
Tom Whittington <46750921+ThomasWhittington@users.noreply.github.com>
2026-01-16 01:35:46 +0000

    Added helper methods for getting night orders

--- Clocktower-UI/src/types/script.ts
+++ Clocktower-UI/src/types/script.ts
@@ -33,6 +33,15 @@ export class Script {
         return this.roles.filter(r => r.type === RoleType.Traveller);
     }
 
+    get firstNightOrder(): Role[] {
+        return this.roles?.filter(r => r.firstNight !== 0 && r.type !== RoleType.Traveller)
+            .sort((a, b) => a.firstNight - b.firstNight);
+    }
+
+    get otherNightOrder(): Role[] {
+        return this.roles?.filter(r => r.otherNight !== 0 && r.type !== RoleType.Traveller)
+            .sort((a, b) => a.otherNight - b.otherNight);
+    }
 
 }
 
