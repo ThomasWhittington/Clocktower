565f4b785e5a010acbf143599b5d7e1ec8f4778c
Tom Whittington <46750921+ThomasWhittington@users.noreply.github.com>
2026-01-19 00:38:47 +0000

    Added new svg for swap

--- Clocktower-UI/src/components/ui/icons/index.ts
+++ Clocktower-UI/src/components/ui/icons/index.ts
@@ -20,3 +20,4 @@ export {default as EveningIcon} from "#/icons/evening.svg?react";
 export {default as NightIcon} from "#/icons/night.svg?react";
 export {default as TownsquareIcon} from "#/icons/townsquare.svg?react";
 export {default as BedIcon} from "#/icons/bed.svg?react";
+export {default as SwapIcon} from "#/icons/swap.svg?react";
