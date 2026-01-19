7ecf41a4bef2e12eb0aff32ca78e2aa337d58433
Tom Whittington <46750921+ThomasWhittington@users.noreply.github.com>
2026-01-19 16:41:10 +0000

    Added role distribution calculations and tests

---
+++ Clocktower.ServerTests/Data/GamePerspectiveTests.cs
@@ -0,0 +1,65 @@
+﻿using Clocktower.Server.Data;
+using Clocktower.Server.Data.Types.Enum;
+
+namespace Clocktower.ServerTests.Data;
+
+[TestClass]
+public class GamePerspectiveTests
+{
+    [TestMethod]
+    [DataRow(0)]
+    [DataRow(1)]
+    [DataRow(2)]
+    [DataRow(3)]
+    [DataRow(4)]
+    public void DefaultRoleDistribution_LessThan5Players_BeNull(int playerCount)
+    {
+        var perspective = GetGamePerspective(playerCount);
+
+        perspective.DefaultRoleDistributions.Should().BeNull();
+    }
+
+    [TestMethod]
+    [DataRow(5, 3, 0, 1, 1)]
+    [DataRow(6, 3, 1, 1, 1)]
+    [DataRow(7, 5, 0, 1, 1)]
+    [DataRow(8, 5, 1, 1, 1)]
+    [DataRow(9, 5, 2, 1, 1)]
+    [DataRow(10, 7, 0, 2, 1)]
+    [DataRow(11, 7, 1, 2, 1)]
+    [DataRow(12, 7, 2, 2, 1)]
+    [DataRow(13, 9, 0, 3, 1)]
+    [DataRow(14, 9, 1, 3, 1)]
+    [DataRow(15, 9, 2, 3, 1)]
+    public void DefaultRoleDistribution_ValidPlayerCounts_CorrectDistribution(
+        int playerCount, int townsfolk, int outsiders, int minions, int demons)
+    {
+        AssertDefaultRoleDistribution(playerCount, new RoleDistribution(townsfolk, outsiders, minions, demons));
+    }
+
+    private static GamePerspective GetGamePerspective(int playerCount)
+    {
+        var players = new List<GameUser>();
+
+        for (int i = 0; i < playerCount; i++)
+        {
+            players.Add(CommonMethods.GetRandomGameUser() with { UserType = UserType.Player });
+        }
+
+        return CommonMethods.GetGamePerspective() with { Users = players };
+    }
+
+    private static void AssertDefaultRoleDistribution(int playerCount, RoleDistribution expectedDistribution)
+    {
+        var roleDistributionTotal = expectedDistribution.Townsfolk + expectedDistribution.Outsiders + expectedDistribution.Minions + expectedDistribution.Demons;
+        roleDistributionTotal.Should().Be(playerCount);
+
+        var perspective = GetGamePerspective(playerCount);
+
+        perspective.DefaultRoleDistributions.Should().NotBeNull();
+        perspective.DefaultRoleDistributions.Townsfolk.Should().Be(expectedDistribution.Townsfolk);
+        perspective.DefaultRoleDistributions.Outsiders.Should().Be(expectedDistribution.Outsiders);
+        perspective.DefaultRoleDistributions.Minions.Should().Be(expectedDistribution.Minions);
+        perspective.DefaultRoleDistributions.Demons.Should().Be(expectedDistribution.Demons);
+    }
+}
