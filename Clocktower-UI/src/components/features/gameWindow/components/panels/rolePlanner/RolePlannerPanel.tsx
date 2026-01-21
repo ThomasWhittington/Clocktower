7b3d5ea140fb20887ea50090376400e88aee9c3d
Tom Whittington <46750921+ThomasWhittington@users.noreply.github.com>
2026-01-21 23:36:53 +0000

    Added basics of role selection

--- Clocktower-UI/src/components/features/gameWindow/components/panels/rolePlanner/RolePlannerPanel.tsx
+++ Clocktower-UI/src/components/features/gameWindow/components/panels/rolePlanner/RolePlannerPanel.tsx
@@ -1,4 +1,18 @@
 ﻿import {BasePanel} from "@/components/ui";
+import {TeamGroup} from "@/components/features/gameWindow/components/panels/rolePlanner/TeamGroup.tsx";
+import {
+    useElementSize,
+    useServerHub
+} from "@/hooks";
+import {
+    Role,
+    RoleType
+} from "@/types";
+import {
+    useMemo,
+    useState
+} from "react";
+import {useDiscordTown} from "@/components/features/discordTownPanel/hooks";
 
 interface RolePlannerPanelProps {
     isOpen: boolean;
@@ -6,9 +20,71 @@ interface RolePlannerPanelProps {
 }
 
 export const RolePlannerPanel = ({isOpen, onClose}: RolePlannerPanelProps) => {
+    const {script} = useServerHub();
+    const {discordTown} = useDiscordTown();
+    const {ref: containerRef, size: parentSize} = useElementSize<HTMLDivElement>();
+    const dynamicSize = Math.min(parentSize.width, parentSize.height) / 8;
+    const [selectedRoles, setSelectedRoles] = useState<Role[]>([]);
+
+    const selectedRoleIds = useMemo(
+        () => new Set(selectedRoles.map(r => r.id)),
+        [selectedRoles]
+    );
+    const tokenClicked = async (role: Role | undefined) => {
+        if (!role) return;
+
+        const isAlreadySelected = selectedRoles.some(r => r.id === role.id);
+        if (isAlreadySelected) {
+            setSelectedRoles(selectedRoles.filter(r => r.id !== role.id));
+        } else {
+            setSelectedRoles([...selectedRoles, role]);
+        }
+    }
+
     return (
         <BasePanel title="Role Planner" isOpen={isOpen} onClose={onClose} className="role-planner">
-            <p>WIP</p>
+            <div ref={containerRef} className="token-panel">
+
+                {selectedRoles.some(o => o.setup) && <p className="select-none">WARNING - Setup is affected</p>}
+                <div className="role-counts">
+                    <span className="role-count townsfolk"><span>{selectedRoles.filter(o => o.type == RoleType.Townsfolk).length}</span>/{discordTown?.defaultRoleDistribution?.townsfolk}</span>
+                    <span className="role-count outsiders"><span>{selectedRoles.filter(o => o.type == RoleType.Outsider).length}</span>/{discordTown?.defaultRoleDistribution?.outsiders}</span>
+                    <span className="role-count minions"><span>{selectedRoles.filter(o => o.type == RoleType.Minion).length}</span>/{discordTown?.defaultRoleDistribution?.minions}</span>
+                    <span className="role-count demons"><span>{selectedRoles.filter(o => o.type == RoleType.Demon).length}</span>/{discordTown?.defaultRoleDistribution?.demons}</span>
+                </div>
+
+                <TeamGroup
+                    name="Townsfolk"
+                    roles={script?.townsfolk}
+                    tokenSize={dynamicSize}
+                    onClick={tokenClicked}
+                    selectedRoleIds={selectedRoleIds}
+                />
+
+                <div className="outsider-minion-demon-group">
+                    <TeamGroup
+                        name="Outsiders"
+                        roles={script?.outsiders}
+                        tokenSize={dynamicSize}
+                        onClick={tokenClicked}
+                        selectedRoleIds={selectedRoleIds}
+                    />
+                    <TeamGroup
+                        name="Minions"
+                        roles={script?.minions}
+                        tokenSize={dynamicSize}
+                        onClick={tokenClicked}
+                        selectedRoleIds={selectedRoleIds}
+                    />
+                    <TeamGroup
+                        name="Demons"
+                        roles={script?.demons}
+                        tokenSize={dynamicSize}
+                        onClick={tokenClicked}
+                        selectedRoleIds={selectedRoleIds}
+                    />
+                </div>
+            </div>
         </BasePanel>
     )
 };
---
+++ Clocktower-UI/src/components/features/gameWindow/components/panels/rolePlanner/TeamGroup.tsx
@@ -0,0 +1,38 @@
+﻿import {Role} from "@/types";
+import {Token} from "@/components/tokens";
+
+interface TokenGroupProps {
+    name: "Townsfolk" | "Outsiders" | "Minions" | "Demons" | "Travellers",
+    roles?: Role[],
+    tokenSize: number,
+    onClick?: (role: Role) => void;
+    selectedRoleIds?: Set<string>;
+}
+
+export const TeamGroup = ({name, roles, tokenSize, onClick, selectedRoleIds}: TokenGroupProps) => {
+    if (roles && roles.length <= 0) return null;
+    const className = `role-group ${name.toLowerCase()}`;
+    return (
+        <>
+            {roles && roles.length > 0 && (
+                <div className={className}>
+                    <h3>{name}</h3>
+                    <div>
+                        {roles.map((role) => {
+                            const isSelected = selectedRoleIds?.has(role.id) ?? false;
+                            return (
+                                <Token
+                                    role={role}
+                                    size={tokenSize}
+                                    key={role.id}
+                                    onClick={() => onClick?.(role)}
+                                    className={isSelected ? 'selected-role' : 'unselected-role'}
+                                />
+                            );
+                        })}
+                    </div>
+                </div>
+            )}
+        </>
+    );
+};
