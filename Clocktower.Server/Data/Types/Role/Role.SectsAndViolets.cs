31d2654cf0135096430607034b2234cd60723fd5
Tom Whittington <46750921+ThomasWhittington@users.noreply.github.com>
2026-01-27 00:53:27 +0000

    Added outsiders, minions, demons and travellers for sects and violets

--- Clocktower.Server/Data/Types/Role/Role.SectsAndViolets.cs
+++ Clocktower.Server/Data/Types/Role/Role.SectsAndViolets.cs
@@ -14,6 +14,9 @@ public partial record Role
     private static Role SectsAndVioletsDemon(string name, string description)
         => Demon(name, description, Edition.SectsAndViolets);
 
+    private static Role SectsAndVioletsTraveller(string name, string description)
+        => Traveller(name, description, Edition.SectsAndViolets);
+
     public static Role Clockmaker => SectsAndVioletsTownsfolk("Clockmaker", "You start knowing how many steps from the demon to it's nearest minion.")
         .FirstNight(41, "Give the Clockmaker the number of steps from the demon to it's nearest minion. A step is a the space between players so a demon with a single player between them and a minion will result in a 2.");
 
@@ -58,4 +61,66 @@ public partial record Role
 
     public static Role Sage => SectsAndVioletsTownsfolk("Sage", "If the Demon kills you, you learn 2 players, one of which is the Demon.")
         .OtherNight(42, "If the sage was killed by a Demon: Give them 2 players, 1 of which is the Demon.");
+
+    public static Role Mutant => SectsAndVioletsOutsider("Mutant", "If you are \"mad\" about being an Outsider, you might be EXECUTED");
+
+    public static Role Barber => SectsAndVioletsOutsider("Barber", "If you died today or tonight, the Demon may choose 2 players (not another Demon) to swap characters.")
+        .OtherNight(40, "If the barber died today, tell the Demon the barber has died. As the demon if they wish to use the ability given by the barber. If yes, they choose 2 players (not another Demon) to swap characters. Wake them players and inform them of their new role. Their alignments are not affected.")
+        .WithReminders("Haircuts tonight");
+
+    public static Role Sweetheart => SectsAndVioletsOutsider("Sweetheart", "When you die, 1 player is drunk from now on.")
+        .OtherNight(41, "If the Sweetheart died, choose a player to be drunk from now on.")
+        .WithReminders("Drunk");
+
+    public static Role Klutz => SectsAndVioletsOutsider("Klutz", "When you learn you died, publicly choose 1 alive player: if they are evil, your team loses.");
+
+    public static Role Witch => SectsAndVioletsMinion("Witch", "Each night, choose a player: if they nominate tomorrow, they die (not executed). If just 3 players live, you loose this ability.")
+        .FirstNight(24, "The Witch chooses a player. Mark them as 'Cursed'")
+        .OtherNight(14, "If there are 4 or more players alive: The Witch chooses a player. Mark them as 'Cursed'")
+        .WithReminders("Cursed");
+
+    public static Role Cerenovus => SectsAndVioletsMinion("Cerenovus", "Each night, choose a player & a good character: they are \"mad\" they are this character tomorrow, or might be executed (even if already dead).")
+        .EachNight(25, 15, "The Cerenovus chooses a player and a good character. Mark them as 'Mad'. Wake the selected player and inform them that the cerenovus selected them. Inform them of their madness. If they are not \"Mad\" tomorrow, they can be executed.")
+        .WithReminders("Mad");
+
+    public static Role PitHag => SectsAndVioletsMinion("Pit-Hag", "Each night*, choose a player and a character they become (if not in play). If a Demon is made, deaths tonight are arbitary.")
+        .OtherNight(16, "The pithag chooses a player and a character. If this character is not in play, wake the selected player and inform them of their new role. Their alignments are not affected.");
+
+    public static Role EvilTwin => SectsAndVioletsMinion("Evil Twin", "You & an opposing player know each other. If the good player is executed, evil wins. Good can't win if you both live.")
+        .FirstNight(23, "Wake the evil twin and inform them of the good twin and the good twins role. Wake the good twin, inform them that they are the good twin and inform them who the evil twin is.")
+        .WithReminders("Twin");
+
+    public static Role FangGu => SectsAndVioletsDemon("Fang Gu", "Each night*, choose a player, they die. The 1st outsider this kills becomes and evil Fang Gu and you die instead. [+1 Outsider]")
+        .OtherNight(29, "The Fang Gu chooses a player. If this player is not an outsider: they die. If this player is an outsider, the Fang Gu dies then wake them and inform them that they are now an evil Fang Gu.")
+        .WithReminders("Dead", "Once")
+        .AffectsSetup();
+
+    public static Role Vigormortis => SectsAndVioletsDemon("Vigormortis", "Each night*, choose a player: they die. Minions you kill keep their ability and poison 1 Townsfolk neighbour. [-1 Outsider]")
+        .OtherNight(32, "The Vigormortis chooses a player: they die. If this player is a minion: they keep their ability and one of their Townsfolk neighours is poisoned")
+        .WithReminders("Dead", "Has Ability", "Poisoned")
+        .AffectsSetup();
+
+    public static Role NoDashii => SectsAndVioletsDemon("No Dashii", "Each night*, choose a player: they die. Your 2 Townsfolk neighbours are poisoned.")
+        .OtherNight(30, "The No Dashii chooses a player: they die. Their 2 Townsfolk neighbours are poisoned.")
+        .WithReminders("Dead", "Poisoned");
+
+    public static Role Vortox => SectsAndVioletsDemon("Vortox", "Each night*, choose a player: they die. Townsfolk abilities yield objectively false information. Each day, if no-one is executed, evil wins. (Executing dead players counts as an execution).")
+        .OtherNight(31, "The Vortox chooses a player: they die.")
+        .WithReminders("Dead");
+
+    public static Role Barista => SectsAndVioletsTraveller("Barista", "Each night, until dusk, 1) a health player becomes sober, healthy and gets true info, or 2) their ability works twice. The selected player learns which.")
+        .EachNight(1, 1, "Choose a player, wake them and tell them which Barista power is affecting them. Treat them accordingly (sober/healthy/true info or activate their ability twice).")
+        .WithReminders("Sober & Healthy", "Abilty Twice");
+
+    public static Role Harlot => SectsAndVioletsTraveller("Harlot", "Each night*, choose a living player: if they agree, you learn their character, but you both might die.")
+        .OtherNight(1, "The Harlot chooses a player. Wake the chosen player, Inform them that the Harlot selected them. Ask if they wish to share their character, If yes: wake the Harlot and show them the chosen player's character token. Then, you may decide that both players die.")
+        .WithReminders("Dead");
+
+    public static Role Butcher => SectsAndVioletsTraveller("Butcher", "Each day, after the first execution, you may nominate again.");
+
+    public static Role BoneCollector => SectsAndVioletsTraveller("Bone Collector", "Once per game, at night*, choose a dead player, they regain their ability until dusk.")
+        .OtherNight(1, "Ask the Bone Collector if they wish to use their ability. If yes: They choose a dead player, 'Has Ability' reminder token. (They may now wake this night if their ability is in the order)")
+        .WithReminders("No Ability", "Has Ability");
+
+    public static Role Deviant => SectsAndVioletsTraveller("Deviant", "If you were funny today, you cannot die by exhile.");
 }
