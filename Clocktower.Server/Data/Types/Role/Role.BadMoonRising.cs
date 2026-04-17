e732781ef67348834aaaa93212213492d4264383
Tom Whittington <46750921+ThomasWhittington@users.noreply.github.com>
2026-04-17 01:02:46 +0100

    Fixed typos

--- Clocktower.Server/Data/Types/Role/Role.BadMoonRising.cs
+++ Clocktower.Server/Data/Types/Role/Role.BadMoonRising.cs
@@ -48,11 +48,11 @@ public partial record Role
 
     public static Role Courtier => BadMoonRisingTownsfolk("Courtier", "Once per game, at night, choose a character: they are drunk for 3 nights & 3 days.")
         .FirstNight(19, "Ask if they would like to use their ability. If yes, they select a character: If that character is in play, that player is drunk.")
-        .OtherNight(8, "Reduce the remaining number of days the marked player is poisoned. If the Courtier has not yet used their ability: Ask if they would like to use their ability. If yes, they select a character: If that character is in play, that player is drunk.")
+        .OtherNight(8, "Reduce the remaining number of days the marked player is drunk. If the Courtier has not yet used their ability: Ask if they would like to use their ability. If yes, they select a character: If that character is in play, that player is drunk.")
         .WithReminders("Drunk 3", "Drunk 2", "Drunk 1", "No Ability");
 
     public static Role Professor => BadMoonRisingTownsfolk("Professor", "Once per game, at night*, choose a dead player: if they are a Townsfolk, they are resurrected.")
-        .OtherNight(43, "If the Professor has not yet used their ability: Ask if they would like to use their ability. If yes, they select a a dead player: If that player is a townsfolk, that player is resurrected.")
+        .OtherNight(43, "If the Professor has not yet used their ability: Ask if they would like to use their ability. If yes, they select a dead player: If that player is a townsfolk, that player is resurrected.")
         .WithReminders("Alive", "No Ability");
 
     public static Role Minstrel => BadMoonRisingTownsfolk("Minstrel", "When a minion dies by execution, all other players (except Travellers) are drunk until dusk tomorrow.")
@@ -79,7 +79,7 @@ public partial record Role
 
     public static Role Lunatic => BadMoonRisingOutsider("Lunatic", "You think you are a Demon, but you are not. The Demon knows who you are & who you choose at night.")
         .FirstNight(8, "If 7 or more players: Show the Lunatic a number of arbitrary 'Minions', players equal to the number of Minions in play. Show 3 character tokens of arbitrary good characters. If the token received by the Lunatic is a Demon that would wake tonight: Allow the Lunatic to do the Demon actions. Place their 'chosen' markers. Wake the Demon. Show the Demon's real character token. Show them the Lunatic player. If the Lunatic attacked players: Show the real demon each marked player. Remove any Lunatic 'chosen' markers.")
-        .OtherNight(20, "Allow the Lunatic to do the actions of the Demon. Place their 'attack' markers. If the Lunatic selected players: Wake the Demon. Show the 'chosen' marker, then point to each marked player. Remove any Lunatic 'chosen' markers.")
+        .OtherNight(20, "Allow the Lunatic to do the actions of the Demon. Place their 'attack' markers. If the Lunatic selected players: Wake the Demon. Show them who the lunatic selected. Remove any Lunatic 'chosen' markers.")
         .WithReminders("Chosen");
 
     public static Role Godfather => BadMoonRisingMinion("Godfather", "You start knowing which Outsiders are in play. If 1 died today, choose a player tonight: they die. [-1 or +1 Outsider]")
@@ -88,13 +88,13 @@ public partial record Role
         .WithReminders("Died Today", "Dead")
         .AffectsSetup();
 
-    public static Role DevilsAdvocate => BadMoonRisingMinion("Devil's Advocate", "Each night, choose a living player (different to last night): if exectuted tomorrow, they don't die.")
-        .FirstNight(22, "The Devil's Advocate selects to a living player. That player survives execution tomorrow.")
-        .OtherNight(13, "The Devil's Advocate selects to a living player, different from the previous night. That player survives execution tomorrow.")
+    public static Role DevilsAdvocate => BadMoonRisingMinion("Devil's Advocate", "Each night, choose a living player (different to last night): if executed tomorrow, they don't die.")
+        .FirstNight(22, "The Devil's Advocate selects a living player. That player survives execution tomorrow.")
+        .OtherNight(13, "The Devil's Advocate selects a living player, different from the previous night. That player survives execution tomorrow.")
         .WithReminders("Survives Execution");
 
     public static Role Assassin => BadMoonRisingMinion("Assassin", "Once per game, at night*, choose a player: they die, even if for some reason they could not.")
-        .OtherNight(36, "If the Assassin has not yet used their ability: The Assassin is asked if they want to use their ability, if yes,they select a player. That player dies.")
+        .OtherNight(36, "If the Assassin has not yet used their ability: The Assassin is asked if they want to use their ability, if yes, they select a player. That player dies.")
         .WithReminders("Dead", "No Ability");
 
     public static Role Mastermind => BadMoonRisingMinion("Mastermind", "If the demon dies by execution (ending the game), play for 1 more day. If a player is then executed, their team loses.");
@@ -108,7 +108,7 @@ public partial record Role
         .OtherNight(27, "One player that the Shabaloth chose the previous night might be resurrected. The Shabaloth selects two players. Those players die.")
         .WithReminders("Dead", "Alive");
 
-    public static Role Po => BadMoonRisingDemon("Po", "Each night*, you may choose a player: they die. If you last choice was no-one, choose 3 players tonight.")
+    public static Role Po => BadMoonRisingDemon("Po", "Each night*, you may choose a player: they die. If your last choice was no-one, choose 3 players tonight.")
         .OtherNight(28, "If the Po chose no-one the previous night: The Po selects three players. Otherwise: The Po either selects a player or declines to use ability. Chosen players die")
         .WithReminders("Dead", "3 Attacks");
 
